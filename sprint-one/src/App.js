8eea26c96ef5f9ddd0c81640094f7d256a68fb6c
Steven Martins <Steven@MacBook-Pro.local>
2020-05-16 02:07:08 -0700

    updates to nav bar design and main video section

--- sprint-one/src/App.js
+++ sprint-one/src/App.js
@@ -1,181 +1,3 @@
-// // import React from "react";
-// // import Header from "./components/Header";
-// // // import MainContent from "./components/MainContent";
-// // // import Footer from "./components/Footer";
-// // export default function App() {
-// //   return (
-// //     <div>
-// //       <Header />
-// //       {/* <MainContent />
-// //       <Footer /> */}
-// //     </div>
-// //   );
-// // }
-
-// import React from "react";
-// // import Header from "./components/Header";
-// // import Video from "./components/Video";
-// // import Description from "./components/Description";
-// // import Comments from "./components/Comments";
-// // import VideoList from "./components/VideosList";
-// import CommentsList from "./components/CommentsList";
-
-// import profile from "./assets/Icons/PNG/userico.png";
-// import logo from "./assets/Logo/brainflix.svg";
-// import uploadIcon from "./assets/Icons/SVG/Icon-upload.svg";
-// import viewsIcon from "./assets/Icons/SVG/Icon-views.svg";
-// import likesIcon from "./assets/Icons/SVG/Icon-likes.svg";
-// import video from "./assets/Video/BrainStationSampleVideo.mp4";
-// import playIcon from "./assets/Icons/SVG/Icon-play.svg";
-// import scrubber from "./assets/Icons/SVG/Icon-scrubber-control.svg";
-// import fullscreenIcon from "./assets/Icons/SVG/Icon-fullscreen.svg";
-// import volumeIcon from "./assets/Icons/SVG/Icon-volume.svg";
-
-// const App = () => {
-//   return (
-//     <div className="site">
-//       <header className="nav-bar" id="nav-bar">
-//         <div className="nav-bar__tablet">
-//           <a className="nav-bar__logo-link" href="index.html">
-//             <img className="nav-bar__logo" src={logo} alt="Brainflix Logo" />
-//           </a>
-//           <nav className="nav-bar__features">
-//             <input
-//               type="text"
-//               id="nav-bar__search"
-//               className="nav-bar__search"
-//               placeholder="Search"
-//               name="search"
-//             />
-
-//             <div className="nav-bar__lower-row">
-//               <button type="submit" className="nav-bar__button">
-//                 <img
-//                   className="nav-bar__button--icon"
-//                   src={uploadIcon}
-//                   alt="Upload Icon"
-//                 />{" "}
-//                 UPLOAD
-//               </button>
-//               <img
-//                 className="nav-bar__profile"
-//                 src={profile}
-//                 alt="User Profile Picture"
-//               />
-//             </div>
-//           </nav>
-//         </div>
-//       </header>
-//       <main className="main-content">
-//         <div className="video">
-//           {/* How do I put the actual video in? */}
-//           <video className="video__hero" poster={video}>
-//             <source src={video} type="video/mp4" />
-//             {/* These icons are not working. Could be due to the video tag not being right. */}
-//             <div className="video-icons">
-//               <img
-//                 className="video-icons__play"
-//                 src={playIcon}
-//                 alt="Play Button"
-//               />
-//               <img
-//                 className="video-icons__scrubber"
-//                 src={scrubber}
-//                 alt="scrubber"
-//               />
-//               <img
-//                 className="video-icons__fullscreen"
-//                 src={fullscreenIcon}
-//                 alt="Fullscreen Button"
-//               />
-//               <img
-//                 className="video-icons__volume"
-//                 src={volumeIcon}
-//                 alt="Volume Button"
-//               />
-//             </div>
-//           </video>
-//         </div>
-//       </main>
-//       <div className="description">
-//         <h1 className="description__title">BMX Rampage: 2018 Highlights</h1>
-//         <div className="description__tablet">
-//           <div className="description__details">
-//             <h2 className="description__author">By Red Cow</h2>
-//             <h2 className="description__date">12/18/2018</h2>
-//           </div>
-//           <div className="description__icons">
-//             <h4 className="description__views">
-//               <img
-//                 className="description__views-icon"
-//                 src={viewsIcon}
-//                 alt="Views Counter"
-//               />
-//               1,001,023
-//             </h4>
-//             <h4 className="description__likes">
-//               <img
-//                 className="description__views-icon"
-//                 src={likesIcon}
-//                 alt="Likes Counter"
-//               />
-//               110,985
-//             </h4>
-//           </div>
-//         </div>
-//         <h4 className="description__content">
-//           On a gusty day in Southern Utah, a group of 25 daring mountain bikers
-//           blew the doors off what is possible on two wheels, unleashing some of
-//           the biggest moments the sport has ever seen. While mother nature only
-//           allowed for one full run before the conditions made it impossible to
-//           ride, that was all that was needed for event veteran Kyle Strait, who
-//           won the event for the second time -- eight years after his first Red
-//           Cow Rampage title
-//         </h4>
-//       </div>
-//       <div className="comments">
-//         <h1 className="comments__title">3 Comments</h1>
-//         <div className="comments__container">
-//           <form id="comments-form" className="comments-form">
-//             <div className="comments-form__row-one">
-//               <img
-//                 className="comments-form__profile"
-//                 src={profile}
-//                 alt="User Profile Picture"
-//               />
-//               <div className="comments-form__input-details">
-//                 <label className="comments-form__input-title">
-//                   JOIN THE CONVERSATION
-//                 </label>
-//                 <textarea
-//                   type="text"
-//                   id="comments-content-input"
-//                   placeholder="Write comment here"
-//                   className="comments-form__content"
-//                   name="commentsComment"
-//                 ></textarea>
-//               </div>
-//             </div>
-
-//             <br />
-//             <button type="submit" className="comments-form__submit">
-//               COMMENT
-//             </button>
-//           </form>
-//         </div>
-//       </div>
-
-//       <CommentsList />
-//       <div className="video-list">
-//         <h4>Next Video</h4>
-//         {/* Videos List */}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { Component } from "react";
 import "./styles/main.css";
 import Header from "./components/Header";
@@ -185,6 +7,7 @@ import Comments from "./components/Comments";
 import CommentsList from "./components/CommentsList";
 import VideoList from "./components/VideosList";
 import uuid from "uuid/v1";
+import imageZero from "./assets/Images/video-list-0.jpg";
 import imageOne from "./assets/Images/video-list-1.jpg";
 import imageTwo from "./assets/Images/video-list-2.jpg";
 import imageThree from "./assets/Images/video-list-3.jpg";
@@ -193,6 +16,7 @@ import imageFive from "./assets/Images/video-list-5.jpg";
 import imageSix from "./assets/Images/video-list-6.jpg";
 import imageSeven from "./assets/Images/video-list-7.jpg";
 import imageEight from "./assets/Images/video-list-8.jpg";
+import video from "./assets/Video/BrainStationSampleVideo.mp4";
 
 export default class App extends Component {
   state = {
@@ -253,13 +77,11 @@ export default class App extends Component {
       description:
         "On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen.While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title",
       channel: "By Red Cow",
-      image: "../assets/Images/video-list-0.jpg",
+      image: imageZero,
       views: " 1,001,023",
       likes: "110,985",
-      //duration?
-      duration: "type of <string>",
-      video: "../assets/Video/BrainStationSampleVideo.mp4",
-      //   convert this to timestamp?
+      duration: "0:00 / 0:42",
+      video: video,
       timestamp: "12/18/2018",
 
       comments: [
@@ -292,7 +114,7 @@ export default class App extends Component {
     return (
       <>
         <Header />
-        <Video />
+        <Video mainVideo={this.state.mainVideo} />
         <section className="wrapper-1">
           <section className="wrapper-2">
             <Description mainVideo={this.state.mainVideo} />
--- sprint-one/src/components/Video.js
+++ sprint-one/src/components/Video.js
@@ -1,38 +1,98 @@
 import React from "react";
 
-import video from "../assets/Video/BrainStationSampleVideo.mp4";
-
 import playIcon from "../assets/Icons/SVG/Icon-play.svg";
 import scrubber from "../assets/Icons/SVG/Icon-scrubber-control.svg";
 import fullscreenIcon from "../assets/Icons/SVG/Icon-fullscreen.svg";
 import volumeIcon from "../assets/Icons/SVG/Icon-volume.svg";
 
-export default function Video() {
+export default function Video({ mainVideo }) {
   return (
-    <div className="video">
-      {/* How do I put the actual video in? */}
-      <video className="video__hero" poster={video}>
-        <source src={video} type="video/mp4" />
-        {/* These icons are not working. Could be due to the video tag not being right. */}
-        <div className="video-icons">
-          <img className="video-icons__play" src={playIcon} alt="Play Button" />
-          <img
-            className="video-icons__scrubber"
-            src={scrubber}
-            alt="scrubber"
-          />
-          <img
-            className="video-icons__fullscreen"
-            src={fullscreenIcon}
-            alt="Fullscreen Button"
-          />
-          <img
-            className="video-icons__volume"
-            src={volumeIcon}
-            alt="Volume Button"
-          />
+    <div className="video-player">
+      <div className="video-player__controls">
+        <button className="video-player__btn">
+          <img className="fas fa-play" src={playIcon} alt="Play Button" />
+          {/* // <i className="fas fa-play"></i> */}
+        </button>
+
+        <div className="video-player__progress-bar">
+          <div className="video-player__progress-timeline">
+            <div className="video-player__progress-scrubber">
+              {" "}
+              <img src={scrubber} alt="Scrubber" />
+            </div>
+          </div>
+          <div className="video-player__progress-text">
+            {mainVideo.duration}
+          </div>
         </div>
-      </video>
+
+        <div className="video-player__btn-group">
+          <button className="video-player__btn">
+            <img
+              className="fas fa-expand"
+              src={fullscreenIcon}
+              alt="Full Screen Button"
+            />
+            {/* <i className="fas fa-expand"></i> */}
+          </button>
+          <button className="video-player__btn">
+            <img
+              className="fas fa-volume-up"
+              src={volumeIcon}
+              alt="Volume Button"
+            />
+            {/* <i className="fas fa-volume-up"></i> */}
+          </button>
+        </div>
+      </div>
+      <video
+        poster={mainVideo.image}
+        className="video-player__video"
+        src={mainVideo.video}
+      ></video>
     </div>
   );
 }
+
+// import React from "react";
+
+// import video from "../assets/Video/BrainStationSampleVideo.mp4";
+
+// // import playIcon from "../assets/Icons/SVG/Icon-play.svg";
+// // import scrubber from "../assets/Icons/SVG/Icon-scrubber-control.svg";
+// // import fullscreenIcon from "../assets/Icons/SVG/Icon-fullscreen.svg";
+// // import volumeIcon from "../assets/Icons/SVG/Icon-volume.svg";
+// import imageZero from "../assets/Images/video-list-0.jpg";
+
+// export default function Video() {
+//   return (
+//     <div className="video-player">
+//       <div className="video-player__controls">
+//         <button className="video-player__btn">
+//           <i className="fas fa-play"></i>
+//         </button>
+
+//         <div className="video-player__progress-bar">
+//           <div className="video-player__progress-timeline">
+//             <div className="video-player__progress-scrubber"></div>
+//           </div>
+//           <div className="video-player__progress-text">0:00 / 0:42</div>
+//         </div>
+
+//         <div className="video-player__btn-group">
+//           <button className="video-player__btn">
+//             <i className="fas fa-expand"></i>
+//           </button>
+//           <button className="video-player__btn">
+//             <i className="fas fa-volume-up"></i>
+//           </button>
+//         </div>
+//       </div>
+//       <video
+//         poster={imageZero}
+//         className="video-player__video"
+//         src={video}
+//       ></video>
+//     </div>
+//   );
+// }
